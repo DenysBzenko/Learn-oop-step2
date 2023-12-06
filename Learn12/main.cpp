8902110bcd8677d029a51b69c0acb4c052074ef0
Denys Bzenko <dbzenko@kse.org.ua>
2023-12-06 11:09:44 +0200

    Code works with all classes good. add dynamic_cast

--- Learn12/main.cpp
+++ Learn12/main.cpp
@@ -1,30 +1,43 @@
 #include<iostream>
 
+using namespace std;
+
 class Figure {
 private:
-    float x;
-    float y;
+    double x;
+    double y;
 
 public:
-	Figure(float  x, float  y) : x(x), y(y) {}
+	Figure(double  x, double  y) : x(x), y(y) {}
 
-    float  getX() const {
+    double  getX() const {
         return x;
     }
 
-    float  getY() const {
+    double  getY() const {
         return y;
     }
 
+    virtual void getSquare() = 0;
+
+    virtual ~Figure() = default;
 };
 
 
-class Rectangle : private Figure {
+class Rectangle : public Figure {
 private:
     int width;
     int height;
 public:
-    Rectangle(float x, float y, float width, float height): Figure(x, y), width(width), height(height) {}
+    Rectangle(double x, double y, int width, int height): Figure(x, y), width(width), height(height) {}
+
+    void getSquare() override {
+        cout << "Square for Rectangle " << width * height << endl;
+    }
+
+    void getPerimeter() {
+        cout << "Perimeter for Rectangle " << 2 * (width + height) << endl;
+    }
 };
 
 
@@ -33,18 +46,59 @@ class Circle : public Figure {
 private:
     int radius;
 public:
-    Circle(float x, float y, int radius): Figure(x, y), radius(radius) {}
+    Circle(double x, double y, int radius): Figure(x, y), radius(radius) {}
+
+    void getSquare() override {
+        cout << "Square for circle " << (2 * radius) * 3.14 << endl;
+    }
+
+    void getPerimeter()  {
+        cout << "Perimetr for circle " << 2 * 3.14 * radius << endl;
+    }
 };
 
 
 
-class Square : private Rectangle {
+class Square : public Rectangle {
 public:
-    Square(float x, float y, int width , int height ): Rectangle(x, y, width, height) {}
+    Square(double x, double y, int size  ): Rectangle(x, y, size, size) {}
+
+    
 };
 
 
 
 int main() {
+    Rectangle rectangle(1.0, 2.0, 3.0, 4.0);
+    Circle circle(5.0, 6.0, 2.5);
+    Square square(8.0, 9.0, 3.0);
 
+    cout << "Rectangle Coordinates: " << rectangle.getX() << ", " << rectangle.getY()<< endl;
+    rectangle.getSquare();
+    rectangle.getPerimeter();
+
+    cout << "Circle Coordinates: " << circle.getX() << ", " << circle.getY() << endl;
+    circle.getSquare();
+    circle.getPerimeter();
+
+    cout << "Square Coordinates: " << square.getX() << ", " << square.getY() << endl;
+    square.getSquare();
+    square.getPerimeter();
+
+    unique_ptr<Figure> rectangle_p(make_unique<Rectangle>(10.0, 11.0, 5, 6));
+
+    Rectangle* castRectangle = dynamic_cast<Rectangle*>(rectangle_p.get());
+    if (castRectangle) {
+        cout << "New Rectangle coordinate: " << castRectangle->getX() << ", " << castRectangle->getY() << endl;
+        castRectangle->getSquare();
+        castRectangle->getPerimeter();
+    }
+    else {
+        cout << "You dont have perimeter" << endl;
+    }
+        
+    delete castRectangle;
+
+ 
+    return 0; 
 }
